9742f3c1e94370f559d023a1548c923577ac2365
DewBM <dewmitha.mahiti@gmail.com>
2023-11-01 02:02:19 +0530

    partial admin-staff crud

--- admin/staff.js
+++ admin/staff.js
@@ -1,3 +1,5 @@
+const url = "http://localhost:8080/railboost_backend_war_exploded/staff";
+
 function updateUsername() {
     const staffId = document.getElementById('staffId').value;
     const name = document.getElementById('name').value;
@@ -16,21 +18,53 @@ function updateUsername() {
 }
 
 function addStaff() {
+    staffMember = {};
+
     const email = document.getElementById('email').value;
     const telephone = document.getElementById('telephone').value;
     const username = document.getElementById('username').value;
 
+    staffMember["staffId"] = document.getElementById('staffId').value;
+    staffMember["station"] = document.getElementById('railwayStation').value;
+    staffMember["fName"] = document.getElementById("fName").value;
+    staffMember["lName"] = document.getElementById("lName").value;
+    staffMember["role"] = document.getElementById("role").value;
+    staffMember["email"] = email;
+    staffMember["telNo"] = telephone;
 
-            alert(`Link to create a password for the Username :${username} has been sent to the email: ${email} and phone number: ${telephone}.`);
+    console.log(staffMember);
+
+
+    const body = staffMember;
+    const params = {
+        headers : {
+            "Content-type": "application/json; charset=UTF-8"
+        },
+        body : JSON.stringify(body),
+        method : "POST",
+        credentials : "include"
+    };
+
+    fetch(url, params)
+    .then(res => {
+        if(res.ok) {
+            window.location.reload();
+        }
+    });
+
+    console.log(train);
+
+
+    alert(`Link to create a password for the Username :${username} has been sent to the email: ${email} and phone number: ${telephone}.`);
     // Clear the form
-    document.getElementById('staffId').value = '';
-    document.getElementById('name').value = '';
-    document.getElementById('role').value = ''; // Reset role to the default value
-    document.getElementById('railwayStation').value = '';
-    document.getElementById('email').value = '';
-    document.getElementById('telephone').value = '';
-    document.getElementById('username').value = '';
+    // document.getElementById('staffId').value = '';
+    // document.getElementById('name').value = '';
+    // document.getElementById('role').value = ''; // Reset role to the default value
+    // document.getElementById('railwayStation').value = '';
+    // document.getElementById('email').value = '';
+    // document.getElementById('telephone').value = '';
+    // document.getElementById('username').value = '';
 
     // Hide the message
-    document.getElementById('message').style.display = 'none';
+    // document.getElementById('message').style.display = 'none';
 }
