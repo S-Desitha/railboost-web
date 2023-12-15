03cce455e2f8d1d5182e25123d27de444714cb7e
DewBM <dewmitha.mahiti@gmail.com>
2023-12-15 12:32:39 +0530

    add code to load navbar in the included page.

--- js/script.js
+++ js/script.js
@@ -1,9 +1,29 @@
+// $(".navbar").on("load", function() {
+//     console.log("Hello JQuery");
+//     $.get("/html/navbar.html", function(data) {
+//         console.log(data);
+//         $("#navbar").html(data);
+//     });
+// });
+
+
+$(".navbar").ready(function() {
+    $.get("/html/navbar.html", function(data) {
+        $(".navbar").html(data);
+    });
+});
+
+
+
+
 function seatReserve() {
     var userResponse = confirm("Now you'll be redirected to the official website for seat reservations of Sri Lanka Railways.");
     if (userResponse) {
         window.location.href = "https://seatreservation.railway.gov.lk/"; 
     }
 }
+
+
 function confirmCall() {
     var userResponse = confirm("You can call this phone number only in the following cases.\nOccurring in a train and within a railway premises\n1. A conflict situation\n2. A theaf or related complaint\n3. Smoking, drug or alcohol use\n4. Harassment or other similar conduct\n5. Misuse of railway property");
     if (userResponse) {
@@ -77,3 +97,11 @@ document.addEventListener("DOMContentLoaded", function(){
         
     }
 });
+
+
+
+// function includeNavbar() {
+//     $.get("/html/navbar.html", function(data) {
+//         $("#navbar").html(data);
+//     });
+// }
