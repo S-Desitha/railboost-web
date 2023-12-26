94d3ea11378f905beb3d2aedb98672f33c69d5d4
DewBM <dewmitha.mahiti@gmail.com>
2023-12-26 12:20:48 +0530

    remove some console.log statements.

--- js/auth.js
+++ js/auth.js
@@ -13,9 +13,7 @@ function manageAccess(resp, resource) {
 
 function authorize(pageUrl){
     const url = "http://localhost:8080/railboost_backend_war_exploded/auth";
-    console.log("Authorizing User");
     resource = pageUrl.split("/")[2]
-    console.log(resource);
     if (resource=="admin" || resource=="sm" || resource=="tco") {
         fetch(url, {
             credentials : "include"
