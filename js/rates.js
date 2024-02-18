cabc78b0ff384f36a981d692329b401b5a04f5d2
Sathira Gardihewa <sathiradesitha@gmail.com>
2024-02-19 01:03:43 +0530

    do get updated in js

--- js/rates.js
+++ js/rates.js
@@ -1,5 +1,34 @@
-const endpoint = "rates"
-
-document.addEventListener("DOMContentLoaded",async function() {
-    
-}
+document.addEventListener("DOMContentLoaded", async function () {
+    const endpoint = "rates";
+  
+    document.getElementById("train-form").reset();
+  
+    try {
+      let data = await customFetch(endpoint, {});
+  
+      data.forEach(train => {
+        let editButton = document.createElement("button");
+        editButton.classList.add("edit-button");
+        editButton.innerHTML = "<i class='fas fa-edit'></i>";
+        editButton.setAttribute("train", JSON.stringify(train));
+        editButton.onclick = editTrain;
+  
+        let deleteButton = document.createElement("button");
+        deleteButton.classList.add("delete-button");
+        deleteButton.innerHTML = "<i class='fas fa-trash'></i>";
+        deleteButton.setAttribute("train", JSON.stringify(train));
+        deleteButton.onclick = deleteTrain;
+  
+        let row = document.getElementById("train_table").insertRow(-1);
+        row.insertCell(0).innerHTML = train.trainId;
+        row.insertCell(1).innerHTML = train.trainType;
+        row.insertCell(2).append(editButton, deleteButton);
+      });
+  
+    }
+    catch(error) {
+      if (error=="login-redirected")
+          localStorage.setItem("last_url", window.location.pathname);
+    }
+  });
+  
--- js/ticketRate.js
+++ js/ticketRate.js
@@ -1,3 +1,43 @@
+const endpoint3 = "rates"
+console.log("Hi from rates js")
+
+document.addEventListener("DOMContentLoaded", async function () {
+  
+    // document.getElementById("rate-form").reset();
+  
+    try {
+      let data = await customFetch(endpoint3, {});
+  
+      data.forEach(rate => {
+        let editButton = document.createElement("button");
+        editButton.classList.add("edit-button");
+        editButton.innerHTML = "<i class='fas fa-edit'></i>";
+        editButton.setAttribute("rate", JSON.stringify(rate));
+        editButton.onclick = editRate;
+  
+        let deleteButton = document.createElement("button");
+        deleteButton.classList.add("delete-button");
+        deleteButton.innerHTML = "<i class='fas fa-trash'></i>";
+        deleteButton.setAttribute("rate", JSON.stringify(rate));
+        deleteButton.onclick = deleteTrain;
+  
+        let row = document.getElementById("rate_table").insertRow(-1);
+        row.insertCell(0).innerHTML = rate.startStation;
+        row.insertCell(1).innerHTML = rate.endStation;
+        row.insertCell(2).innerHTML = rate.firstClass;
+        row.insertCell(3).innerHTML = rate.secondClass;
+        row.insertCell(4).innerHTML = rate.thirdClass;
+        row.insertCell(5).append(editButton, deleteButton);
+      });
+  
+    }
+    catch(error) {
+      if (error=="login-redirected")
+          localStorage.setItem("last_url", window.location.pathname);
+    }
+  });
+  
+
 function validateStation(){
   
     let Start=document.getElementById("from").getAttribute("stationCode");
