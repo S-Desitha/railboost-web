fcf76d304d2254b49da9ee6dbf3b1556011e82e0
Kalindu Sankalpa <kalindusankalpa5285@gmail.com>
2024-02-26 12:54:34 +0530

    calculating total price function and update html and css

--- js/season.js
+++ js/season.js
@@ -20,8 +20,8 @@ function validateStation(){
 function appplySeasonTicket() {
   season = {};
 
-  season["startStation"] = document.getElementById("from").getAttribute("stationName");
-  season["endStation"] = document.getElementById("to").getAttribute("stationName");
+  season["startStation"] = document.getElementById("from").getAttribute("stationCode");
+  season["endStation"] = document.getElementById("to").getAttribute("stationCode");
   season["passengerType"] = document.getElementById("passenger-type").value;
   season["startDate"] = new Date(document.getElementById("Start-date").value).toLocaleDateString("en-US", {year:"numeric", month:"2-digit", day:"2-digit"})
   season["duration"] = document.getElementById("duration").value;
@@ -60,15 +60,6 @@ function appplySeasonTicket() {
     alert("Season application is recieved.");
     location.reload();
 
-    // // document.getElementById("from").value="null";
-    // document.getElementById("passenger-type").value="";
-    // document.getElementById("Start-date").value="";
-    // document.getElementById("duration").value="";
-    // document.getElementById("End-date").value="";
-    // document.getElementById("class").value="";
-    // document.getElementById("total-price").value="";
-    // document.getElementById("img").value="";
-
   }
 
 }
@@ -87,10 +78,11 @@ function getPrice(){
 
 
 async function getTicketPrices(Class,Duration) {
+  let price;
   var PriceOfOne=document.getElementById("price-of-one");
   var Start = document.getElementById("from").getAttribute("stationCode"); 
   var End = document.getElementById("to").getAttribute("stationCode"); 
-  var Duration = document.getElementById("duration");
+  var type = document.getElementById("passenger-type").value;
   console.log(Start);
   let params = {
       startStation: Start,
@@ -105,14 +97,41 @@ async function getTicketPrices(Class,Duration) {
     console.log(urlQuery);
     if (Class == "1st Class"){
       PriceOfOne.innerHTML = "Price of one ticket is "+data.firstClass+".";
-      document.getElementById("total-price").value = data.firstClass;
+      // document.getElementById("total-price").value = data.firstClass;
+      price = data.firstClass*60;
     }else if(Class == "2nd Class"){
       PriceOfOne.innerHTML = "Price of one ticket is "+data.secondClass+".";
-      document.getElementById("total-price").value = data.secondClass;
+      // document.getElementById("total-price").value = data.secondClass;
+      price = data.secondClass*60;
     }else if(Class == "3rd Class"){
       PriceOfOne.innerHTML = "Price of one ticket is "+data.thirdClass+".";
-      document.getElementById("total-price").value = data.thirdClass;
+      // document.getElementById("total-price").value = data.thirdClass;
+      price = data.thirdClass*60;
     }
+
+    if (Duration == "1 Week"){
+      price = ((price/100)*120)/4;
+    }else if(Duration == "1 Month"){
+      price = price;
+    }else if(Duration == "3 Month"){
+      price = price*3;
+    }
+    // else if(Duration == "6 Month"){
+    //   price = price*6;
+    // }
+
+    if (type == "Normal Passenger"){
+      price = Math.ceil(((price/100)*40)/100)*100;
+    }else if(type == "Government Servant"){
+      price = Math.ceil(((price/100)*15)/100)*100;
+    }else if(type == "Student 1"){
+      price = Math.ceil(((price/100)*5)/50)*50;
+    }else if(type == "Student 2"){
+      price = Math.ceil(((price/100)*10)/50)*50;
+    }else if(type == "Railway Servant"){
+      price = Math.ceil(((price/100)*3)/100)*100;
+    }
+    document.getElementById("total-price").value = price;
   }
   catch (error) {
     console.log("Error fetching ticket price: " + error);
