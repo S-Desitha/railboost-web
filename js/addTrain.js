00b8e6cbbbd61f8a34b16c016c682d066d1eba23
pehan <pehamrathnayake159@gmail.com>
2024-04-22 05:58:50 +0530

    implement parcel add to train feature

--- js/addTrain.js
+++ js/addTrain.js
@@ -1,4 +1,5 @@
 const addTrainsEndPoint = "addParcelsToTrain";
+const addedParcelsArray= [];
 
 document.addEventListener("DOMContentLoaded", async function () {
      getSchedule();
@@ -48,18 +49,72 @@ async function searchParcels(){
             let addButten = document.createElement("button");
            
             addButten.classList.add("set-station-button");
-            addButten.innerHTML = "<i title='addParcel' ><span> Add To Schedule</span></i>";
-            addButten.setAttribute("parcelSchedule", JSON.stringify(sch));
-            addButten.onclick = setSchedule;
+            addButten.innerHTML = "<i title='addParcel' ><span> Add </span></i>";
+            addButten.setAttribute("addedParcel", JSON.stringify(sch));
+            addButten.onclick = addParcelSet;
 
             let row = document.getElementById("addParcels").insertRow(-1);
             row.insertCell(0).innerHTML = sch.bookingId;
-            row.insertCell(1).append(addButten);   
+            row.insertCell(1).innerHTML = sch.item;
+            row.insertCell(2).append(addButten);   
     
         });
     }catch{
 
     }
+
+}
+
+async function addParcelSet(){
+    var addedParcel = JSON.parse(this.getAttribute("addedParcel"));
+
+    addedParcelsArray.push(addedParcel.bookingId);
+    // addedParcelsArray.push(scheduleId);
+    // const newArray = newArray.concat(addedParcelsArray);
+    // let newArray = [].concat(Array, addedParcel)
+    console.log(addedParcelsArray);
+
+
+    // const body = addedParcelsArray;
+    // const params = {
+    //     scheduleId: scheduleId,
+    //     headers: {
+    //     "Content-type": "application/json; charset=UTF-8"
+    //     },
+    //     body: JSON.stringify(body),
+    //     method: "PUT"
+    // };
+    // customFetch(addTrainsEndPoint, params)
+    //     .then(()=> window.location.reload())
+    //     .catch((error) => {
+    //         if (error=="login-redirected")
+    //             localStorage.setItem("last_url", window.location.pathname);
+    //     });
+
+
+}
+
+async function addDileveryStatus(){
+    var scheduleId = document.getElementById("scheduleDropDown").value;
+        const body = {bookingIdList:addedParcelsArray,
+                      scheduleId : scheduleId
+                        
+                    };
+        const params = {
+        sid: scheduleId,
+        headers: {
+        "Content-type": "application/json; charset=UTF-8"
+        },
+        body: JSON.stringify(body),
+        method: "PUT"
+    };
+    customFetch(addTrainsEndPoint, params)
+        .then(()=> window.location.reload())
+        .catch((error) => {
+            if (error=="login-redirected")
+                localStorage.setItem("last_url", window.location.pathname);
+        });
+
 
 }
 
