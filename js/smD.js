d0e2a253417de610c3f7cae2435eba833a82258b
Kalindu Sankalpa <kalindusankalpa5285@gmail.com>
2024-04-28 23:29:12 +0530

    parcel delivery confirm after train arrived, station wrappers, SM dashboard

--- js/smD.js
+++ js/smD.js
@@ -1,6 +1,8 @@
 const endpoint2 = "profile"
 
 document.addEventListener("DOMContentLoaded", async function () {
+    createJourneys();
+    getSMStation();
     // const endpoint3 = "user"
     let params = {
         view: 1,
@@ -16,3 +18,49 @@ document.addEventListener("DOMContentLoaded", async function () {
         localStorage.setItem("roleId", data.role.roleId);
         console.log(data.homeStation);
 });
+
+
+
+
+async function createJourneys() {
+    const endpoint = "journey";
+    let param = {
+        date: new Date().toLocaleDateString("en-US", {year:"numeric", month:"2-digit", day:"2-digit"})
+    }
+    // Get the journeys from the backend
+    let urlQuery = endpoint+`?json=${encodeURIComponent(JSON.stringify(param))}`;
+
+    try {
+        let data = await customFetch(urlQuery, {});
+        
+        console.log(data);
+        // Create a table row for each journey
+        if (Object.keys(data)){
+            data.forEach(journey => {
+                let row = document.getElementById("trainLive_table").insertRow(-1);
+                row.insertCell(0).innerHTML = journey.scheduleId;
+                row.insertCell(1).innerHTML = journey.schedule.trainId;
+                row.insertCell(2).innerHTML = journey.schedule.startStationName;
+                row.insertCell(3).innerHTML = journey.schedule.endStationName;
+            });
+        }           
+    } catch(error) {
+        if (error=="login-redirected")
+            localStorage.setItem("last_url", window.location.pathname);
+    }
+}
+
+async function getSMStation(){
+    const endpoint3="staff";
+
+    let params = {
+        view: 1,
+    };
+    let queryString = Object.keys(params).map(key => key + '=' + encodeURIComponent(params[key])).join('&');
+    let urlQuery = `${endpoint3}?${queryString}`;
+    
+        let data = await customFetch(urlQuery, {credentials: "include"});
+        console.log(data);
+        document.querySelector('.hisstation p').textContent="  "+data.stationName;
+        console.log(data.stationName);
+}
